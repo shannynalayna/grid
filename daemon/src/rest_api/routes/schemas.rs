c3c359fd3f5961766086332bffe587d70e407c68
Shannyn Telander <telander@bitwise.io>
2019-04-30 15:14:51 -0500

    Add handler for ListGridSchemas
    Adds a message, ListGridSchemas, and a handler for this message
    to be used to list all grid schemas from the reporting database.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- daemon/src/rest_api/routes/schemas.rs
+++ daemon/src/rest_api/routes/schemas.rs
@@ -12,8 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::database::models::GridSchema;
+use crate::database::{helpers as db, models::GridSchema};
+use crate::rest_api::{error::RestApiResponseError, routes::DbExecutor};
 
+use actix::{Handler, Message, SyncContext};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -30,5 +32,23 @@ impl GridSchemaSlice {
             description: schema.description.clone(),
             owner: schema.owner.clone(),
         }
+    }
+}
+
+struct ListGridSchemas;
+
+impl Message for ListGridSchemas {
+    type Result = Result<Vec<GridSchemaSlice>, RestApiResponseError>;
+}
+
+impl Handler<ListGridSchemas> for DbExecutor {
+    type Result = Result<Vec<GridSchemaSlice>, RestApiResponseError>;
+
+    fn handle(&mut self, _msg: ListGridSchemas, _: &mut SyncContext<Self>) -> Self::Result {
+        let fetched_schemas = db::list_grid_schemas(&*self.connection_pool.get()?)?
+            .iter()
+            .map(|schema| GridSchemaSlice::from_schema(schema))
+            .collect();
+        Ok(fetched_schemas)
     }
 }
