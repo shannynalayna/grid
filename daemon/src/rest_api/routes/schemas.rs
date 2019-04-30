df17a62b59efdc8d93734794187da1b655b62276
Shannyn Telander <telander@bitwise.io>
2019-04-30 14:46:49 -0500

    Add GridSchemaSlice struct
    Adds a GridSchemaSlice which is able to hold all pertinent information
    from a GridSchema object, without exposing internal details pertaining
    to block numbers and internal record ids.
    
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

---
+++ daemon/src/rest_api/routes/schemas.rs
@@ -0,0 +1,34 @@
+// Copyright 2019 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::database::models::GridSchema;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GridSchemaSlice {
+    pub name: String,
+    pub description: String,
+    pub owner: String,
+}
+
+impl GridSchemaSlice {
+    pub fn from_schema(schema: &GridSchema) -> Self {
+        Self {
+            name: schema.name.clone(),
+            description: schema.description.clone(),
+            owner: schema.owner.clone(),
+        }
+    }
+}
